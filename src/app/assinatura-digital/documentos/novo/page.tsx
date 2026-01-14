8473045a48f50f92f07547da6c3a2289dc7c287b
Jordan Medeiros <jordan.medeiros@sinesys.com.br>
2026-01-14 15:33:42 -0300

    refactor: Update page components to asynchronously resolve searchParams.

--- src/app/assinatura-digital/documentos/novo/page.tsx
+++ src/app/assinatura-digital/documentos/novo/page.tsx
@@ -1,10 +1,9 @@
 import { redirect } from "next/navigation";
 import { buildRedirectUrl, type SearchParams } from "../../redirect-utils";
 
-export default function AssinaturaDigitalDocumentosNovoAliasPage({
-  searchParams,
-}: {
-  searchParams?: SearchParams;
+export default async function AssinaturaDigitalDocumentosNovoAliasPage(props: {
+  searchParams: Promise<SearchParams>;
 }) {
+  const searchParams = await props.searchParams;
   redirect(buildRedirectUrl("/app/assinatura-digital/documentos/novo", searchParams));
 }
