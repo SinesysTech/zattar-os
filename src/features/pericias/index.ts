1a3d419136861c9ef3788ce40faa2adb3c3d3c49
JOrdan Medeiros <jordan.medeiros@sinesys.com.br>
2026-02-15 08:22:28 -0300

    refactor: simplify PericiasContent by delegating view logic to dedicated wrapper components and using new data hooks.

--- src/features/pericias/index.ts
+++ src/features/pericias/index.ts
@@ -4,11 +4,18 @@ export * from './service';
 // Use o service para operações públicas
 export * from './actions/pericias-actions';
 
+// Hooks
+export { usePericias } from './hooks/use-pericias';
+export { useEspecialidadesPericias } from './hooks/use-especialidades-pericias';
+export { usePeritos } from './hooks/use-peritos';
+
 // Components
 export { PericiasContent } from './components/pericias-content';
 export { PericiasTableWrapper } from './components/pericias-table-wrapper';
+export { PericiasListWrapper } from './components/pericias-list-wrapper';
+export { PericiasMonthWrapper } from './components/pericias-month-wrapper';
+export { PericiasYearWrapper } from './components/pericias-year-wrapper';
+export { PericiasListFilters } from './components/pericias-list-filters';
 export { PericiasCalendarMonth } from './components/pericias-calendar-month';
 export { PericiasCalendarYear } from './components/pericias-calendar-year';
 export { PericiaDetalhesDialog } from './components/pericia-detalhes-dialog';
-
-
