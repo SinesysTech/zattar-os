1a914716c662b5e4d5554e057e6c1dbe39ff7bc7
JOrdan Medeiros <jordan.medeiros@sinesys.com.br>
2026-02-17 11:52:38 -0300

    feat: import 2FA configuration loader for the status route.

--- src/app/api/twofauth/status/route.ts
+++ src/app/api/twofauth/status/route.ts
@@ -8,6 +8,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { authenticateRequest } from "@/lib/auth/api-auth";
 import { checkConnection, getUser, TwoFAuthError } from "@/lib/integrations/twofauth/";
+import { load2FAuthConfig } from "@/lib/integrations/twofauth/config-loader";
 
 /**
  * @swagger
