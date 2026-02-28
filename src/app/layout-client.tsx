2de8b17c70136fb9820f5443e1110d59b1f61ab0
JOrdan Medeiros <jordan.medeiros@sinesys.com.br>
2026-02-28 16:57:37 -0300

    fix: add suppressHydrationWarning to style element in RootLayoutClient

--- src/app/layout-client.tsx
+++ src/app/layout-client.tsx
@@ -23,7 +23,7 @@ export default function RootLayoutClient({
 
   return (
     <>
-      <style nonce={nonce} dangerouslySetInnerHTML={{__html: `
+      <style nonce={nonce} suppressHydrationWarning dangerouslySetInnerHTML={{__html: `
         @import url('https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap');
       `}} />
       <ThemeProvider
