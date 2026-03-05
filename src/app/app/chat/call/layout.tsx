9a63263d36f09d9c5207c1358f6adf6bab24a8cd
JOrdan Medeiros <jordan.medeiros@sinesys.com.br>
2026-03-05 16:36:47 -0300

    feat: wrap CallLayout component with UserProvider for context management

--- src/app/app/chat/call/layout.tsx
+++ src/app/app/chat/call/layout.tsx
@@ -1,7 +1,13 @@
+"use client";
+
+import { UserProvider } from "@/providers/user-provider";
+
 export default function CallLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="h-screen w-screen bg-black">
-      {children}
-    </div>
+    <UserProvider>
+      <div className="h-screen w-screen bg-black">
+        {children}
+      </div>
+    </UserProvider>
   );
 }
