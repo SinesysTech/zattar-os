272001943233064b5c683e4a24897bcd9a2e7370
Jordan Medeiros <jordan.medeiros@sinesys.com.br>
2026-04-16 14:32:15 -0300

    chore(rota-assinatura): delete legacy shells (PublicPageShell, PublicStepLayout, SelfieStep, PublicDocumentCard) and clean barrel exports

--- src/app/(assinatura-digital)/assinatura/[token]/_components/index.ts
+++ src/app/(assinatura-digital)/assinatura/[token]/_components/index.ts
@@ -1,41 +1,22 @@
-// Layout components
-export { PublicPageShell } from "./layout/PublicPageShell";
-export { PublicStepLayout } from "./layout/PublicStepLayout";
-export { PublicStepIndicator } from "./layout/PublicStepIndicator";
-
-// Shared components
-export { PublicDocumentCard } from "./shared/PublicDocumentCard";
-export { PublicProgressBar } from "./shared/PublicProgressBar";
-
 // Step components
 export {
   WelcomeStep,
   ConfirmDetailsStep,
   ReviewDocumentStep,
-  SelfieStep,
   SignatureStep,
   SuccessStep,
-} from "./steps";
+} from './steps'
 
 // Flow components
-export { PublicSignatureFlow } from "./PublicSignatureFlow";
-export { PublicSignatureProvider, usePublicSignature } from "./PublicSignatureContext";
-export type { PublicContext, PublicSignatureState } from "./PublicSignatureContext";
+export { PublicSignatureFlow } from './PublicSignatureFlow'
+export { PublicSignatureProvider, usePublicSignature } from './PublicSignatureContext'
+export type { PublicContext, PublicSignatureState } from './PublicSignatureContext'
 
 // Re-export types
-export type { PublicPageShellProps } from "./layout/PublicPageShell";
-export type { PublicStepLayoutProps } from "./layout/PublicStepLayout";
-export type {
-  PublicStepIndicatorProps,
-  PublicStepIndicatorStep,
-} from "./layout/PublicStepIndicator";
-export type { PublicDocumentCardProps } from "./shared/PublicDocumentCard";
-export type { PublicProgressBarProps } from "./shared/PublicProgressBar";
 export type {
   WelcomeStepProps,
   ConfirmDetailsStepProps,
   ReviewDocumentStepProps,
-  SelfieStepProps,
   SignatureStepProps,
   SuccessStepProps,
-} from "./steps";
+} from './steps'
--- src/app/(assinatura-digital)/assinatura/[token]/_components/steps/index.ts
+++ src/app/(assinatura-digital)/assinatura/[token]/_components/steps/index.ts
@@ -1,15 +1,13 @@
 // Step components for public signature flow
-export { WelcomeStep } from "./WelcomeStep";
-export { ConfirmDetailsStep } from "./ConfirmDetailsStep";
-export { ReviewDocumentStep } from "./ReviewDocumentStep";
-export { SelfieStep } from "./SelfieStep";
-export { SignatureStep } from "./SignatureStep";
-export { SuccessStep } from "./SuccessStep";
+export { WelcomeStep } from './WelcomeStep'
+export { ConfirmDetailsStep } from './ConfirmDetailsStep'
+export { ReviewDocumentStep } from './ReviewDocumentStep'
+export { SignatureStep } from './SignatureStep'
+export { SuccessStep } from './SuccessStep'
 
 // Re-export types
-export type { WelcomeStepProps } from "./WelcomeStep";
-export type { ConfirmDetailsStepProps } from "./ConfirmDetailsStep";
-export type { ReviewDocumentStepProps } from "./ReviewDocumentStep";
-export type { SelfieStepProps } from "./SelfieStep";
-export type { SignatureStepProps } from "./SignatureStep";
-export type { SuccessStepProps } from "./SuccessStep";
+export type { WelcomeStepProps } from './WelcomeStep'
+export type { ConfirmDetailsStepProps } from './ConfirmDetailsStep'
+export type { ReviewDocumentStepProps } from './ReviewDocumentStep'
+export type { SignatureStepProps } from './SignatureStep'
+export type { SuccessStepProps } from './SuccessStep'
