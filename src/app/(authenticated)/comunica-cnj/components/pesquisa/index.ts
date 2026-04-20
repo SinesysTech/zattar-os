5dcb8e64959deccae4120a60593804b2e076ec79
Jordan Medeiros <jordan.medeiros@sinesys.com.br>
2026-04-20 17:22:17 -0300

    feat: add SearchResults component for displaying live search results

---
+++ src/app/(authenticated)/comunica-cnj/components/pesquisa/index.ts
@@ -0,0 +1,5 @@
+export { SearchHero } from './search-hero';
+export { SearchQuickFilters } from './search-quick-filters';
+export { SearchShortcuts } from './search-shortcuts';
+export { SearchStats } from './search-stats';
+export { SearchResults } from './search-results';
